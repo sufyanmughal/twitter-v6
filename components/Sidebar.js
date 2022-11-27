395deae7094b9f4c44b9a00114a6c313dfd7ca4e
Abu sufyan <captainsufyan158005@gmail.com>
2022-11-27 15:35:28 -0500

    news api section is completed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ import { DotsCircleHorizontalIcon } from "@heroicons/react/outline";
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image
           width={50}
--- pages/Widgets.js
+++ pages/Widgets.js
@@ -1,6 +1,9 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import News from "../components/News";
+import { useState } from "react";
 
-export default function Widgets() {
+export default function Widgets({ newsResults }) {
+  const [articleNum, setArticleNum] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-5 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky t-0 bg-white py-1.5 z-50">
@@ -12,6 +15,18 @@ export default function Widgets() {
             placeholder="Search Twitter"
           />
         </div>
+      </div>
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4 ">What's happening</h4>
+        {newsResults.slice(0, articleNum).map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button
+          onClick={() => setArticleNum(articleNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show More
+        </button>
       </div>
     </div>
   );
